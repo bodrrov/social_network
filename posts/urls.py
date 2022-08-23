79c56db1493cbdb9938ad1af82ab9ffcc9265b40
bodrrov <80098406+bodrrov@users.noreply.github.com>
2022-08-23 16:51:10 +0300

    +paginator

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("group/<slug:slug>/", views.group_posts),
-    path("new_post/", views.PostNew.as_view(), name = 'new'),
+    path("new_post/", views.new_post, name = 'new'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,21 +4,44 @@ from .models import Post,Group
 from django.contrib.auth.decorators import login_required
 #from .forms import CreationForm
 from .forms import PostForm
+from django.core.paginator import Paginator
 
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
-    return render(request, "index.html", {"posts": latest})
+    post_list = Post.objects.order_by('-pub_date').all()
+    paginator = Paginator(post_list, 10)  # показывать по 10 записей на странице.
 
+    page_number = request.GET.get('page')  # переменная в URL с номером запрошенной страницы
+    page = paginator.get_page(page_number)  # получить записи с нужным смещением
+    return render(
+        request,
+        'index.html',
+        {'page': page, 'paginator': paginator}
+    )
 def group_posts(request, slug=None):
     group = get_object_or_404(Group, slug = slug)
     posts = Post.objects.filter(group = group).order_by("-pub_date")[:12]
+    paginator = Paginator(posts, 10)  # показывать по 10 записей на странице.
+
+    page_number = request.GET.get('page')  # переменная в URL с номером запрошенной страницы
+    page = paginator.get_page(page_number)  # получить записи с нужным смещением
     context = {"group": group, "posts": posts}
 
-    return render(request, "group.html",context)
+    return render(request, "group.html",context, {'page': page, 'paginator': paginator})
 
 #@login_required()
-class PostNew(CreateView):
-    form_class = PostForm
-    success_url = "/"
-    template_name = "new.html"
+#class PostNew(CreateView):
+    #form_class = PostForm
+    #success_url = "/"
+    #template_name = "new.html"
+
+@login_required()
+def new_post(request):
+    form = PostForm(request.POST or None)
+    if not form.is_valid():
+        return render(request, 'new.html', {'form': form})
+    post = form.save(commit=False)
+    post.author = request.user
+    post.save()
+    return redirect('index')
+
